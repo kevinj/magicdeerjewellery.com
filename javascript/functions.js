1d29eb31b93609bb5c0b6903d54e5734512b6906
Kevin Jones <kvn.jns@gmail.com>
2009-02-17 00:55:05 -0800

    Fix rollover behaviour

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,13 +1,22 @@
-function showSection (selector) {
+function fadeToSection (sel) {
     if (jQuery('.section:visible').size()) {
         jQuery('.section:visible').fadeOut(function () {
-            jQuery(selector).fadeIn();
+            jQuery('div'+sel).fadeIn();
         });
     }
     else {
-        jQuery(selector).fadeIn();
+        jQuery('div'+sel).fadeIn();
     }
 }
+function showSection (sel, nofade) {
+    if ($('img'+sel).size()) {
+        $('img'+sel).click();
+    }
+    else {
+        fadeToSection(sel);
+    }
+}
+
 function setPage (page) {
     jQuery('.rollover img').each(function (){
         var $this = $(this);
@@ -17,3 +26,24 @@ function setPage (page) {
         $this.mouseout();
     });
 }
+$(function () {
+    $('.rollover img')
+        .mouseover(function (){
+            var type = this.src.replace(/.*\./, '');
+            var name = $(this).attr('name') + '_hover';
+            this.src = 'images/' + name + '.' + type;
+        })
+        .mouseout(function (){
+            var type = this.src.replace(/.*\./, '');
+            var name = $(this).attr('name');
+            if ($(this).attr('selected')) name += '_hover';
+            this.src = 'images/' + name + '.' + type;
+        })
+        .click(function (){
+            $('.rollover img').attr('selected', '').mouseout();
+            $(this).attr('selected', true).mouseover();
+            if ($(this).attr('showSection')) {
+                fadeToSection('.' + this.className);
+            }
+        });
+});
